6fea0cf172d016993eba9a107f4ddecee1547e49
NaveedG <nganjani@protonmail.com>
2019-10-15 10:47:53 -0500

    code challenge

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,8 +1,36 @@
 class Restaurant
   attr_reader :name
+  @@all = []
 
   def initialize(name)
     @name = name
+    @@all << self
+  end
+  
+  def self.all
+    @@all
+  end
+
+  def reviews
+    Review.all.select { |review| review.restaurant == self }
+  end
+
+  def customers
+    customers = self.reviews.map { |review| review.customer }
+    return customers.uniq
+  end
+
+  def average_star_rating
+    ratings = self.reviews.map { |review| review.rating }
+    return ( ratings.sum / ratings.size )
+  end
+
+  def longest_review
+    return self.reviews.max { |x, y| x.content.length <=> y.content.length }
+  end
+
+  def self.find_by_name(name)
+    self.all.find { |restaurant| restaurant.name == name }
   end
 
 end
--- tools/console.rb
+++ tools/console.rb
@@ -6,6 +6,14 @@ end
 # Insert code here to run before hitting the binding.pry
 # This is a convenient place to define variables and/or set up new object instances,
 # so they will be available to test and play around with in your console
+zayd = Customer.new('Zayd', 'Alawi')
+amr = Customer.new('Amr', 'Alawi')
+sabri = Restaurant.new('Sabri Nehari')
+kabob = Restaurant.new('Kabob House')
+zayd.add_review(sabri, 5, 'wonderful spot')
+zayd.add_review(kabob, 4, 'this restaurant is good for the most part')
+amr.add_review(sabri, 3, 'this place is alright')
+amr.add_review(kabob, 3, 'meh')
 
 binding.pry
 0 #leave this here to ensure binding.pry isn't the last line
